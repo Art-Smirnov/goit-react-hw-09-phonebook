a2b5a121fc846d5fef55cedc499278201f8a75c7
Artem Smirnov <aratatem2013@gmail>
2021-08-15 11:31:09 +0300

    refactor: refact some animations

--- src/views/ContactsView/ContactList/ContactListItem/ContactListItem.js
+++ src/views/ContactsView/ContactList/ContactListItem/ContactListItem.js
@@ -1,8 +1,10 @@
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useTween } from 'react-use';
+
 import { Button } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { gsap, Power3 } from 'gsap/all';
+import { useEffect } from 'react';
 
 import {
   contactsSelectors,
@@ -11,9 +13,17 @@ import {
 import './ContactListItem.scss';
 
 export default function ContactListItem() {
-  const value = useTween('elastic', 1000, 500);
   const dispatch = useDispatch();
   const contacts = useSelector(contactsSelectors.getVisibleContacts);
+
+  useEffect(() => {
+    gsap.fromTo(
+      '.ContactListItem',
+      1,
+      { x: -200, ease: Power3.easeInOut() },
+      { x: 0, ease: Power3.easeInOut() },
+    );
+  }, []);
 
   const onDelete = useCallback(
     id => {
@@ -23,11 +33,7 @@ export default function ContactListItem() {
   );
 
   return contacts.map(({ id, name, number }) => (
-    <li
-      style={{ marginLeft: value * 200 - 200 }}
-      className="ContactListItem"
-      key={id}
-    >
+    <li className="ContactListItem" key={id}>
       <span className="ContactListItem__data">
         {name}: {number}
       </span>
--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,6 +1,47 @@
-import { useTween } from 'react-use';
+import { useEffect, useRef } from 'react';
+import { TimelineLite } from 'gsap/all';
+
+import { textAnimation } from '../../helpers/animate-text';
+import './HomeView.scss';
 
 export default function HomeView() {
-  const value = useTween('elastic', 1000, 500);
-  return <h1 style={{ marginLeft: value * 200 - 200 }}>Home Page</h1>;
+  let tittleTextRef = useRef(null);
+  let titleRef = useRef(null);
+  let textRef = useRef(null);
+
+  useEffect(() => {
+    textAnimation(tittleTextRef);
+  }, []);
+
+  useEffect(() => {
+    const t1 = new TimelineLite({ delay: 0.3 });
+
+    t1.fromTo(titleRef, 1, { y: -500 }, { y: 0 }).fromTo(
+      textRef,
+      1,
+      {
+        transform: 'scale(0.1)',
+        opacity: 0,
+      },
+      {
+        transform: 'scale(1)',
+        opacity: 1,
+      },
+    );
+  }, []);
+  return (
+    <>
+      <h1 className="title" ref={el => (titleRef = el)}>
+        <div ref={el => (tittleTextRef = el)}>Home Page</div>
+      </h1>
+      <p className="home-page-info" ref={el => (textRef = el)}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+        quidem quod corporis rem, vero cupiditate repellendus nobis iste est
+        ullam quae? Possimus ea tempore aliquam consectetur saepe dolor in quod
+        sunt autem totam nihil, repudiandae tenetur quam veniam similique sequi
+        quia aut et, sapiente mollitia. Quia placeat reiciendis numquam
+        deleniti.
+      </p>
+    </>
+  );
 }
